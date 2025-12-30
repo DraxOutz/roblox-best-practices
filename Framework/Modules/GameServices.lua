f7ef246a228b1be3d49801fbfc83de2501b86196
Ryan Rocha <kayquesacanaje@gmail.com>
2025-12-30 13:05:22 -0300

    Update GameServices.lua

--- Framework/Modules/GameServices.lua
+++ Framework/Modules/GameServices.lua
@@ -1,12 +1,34 @@
 --!strict
+-- GameServices.lua
+-- Centraliza todos os serviços utilitários do framework
 
 local GameServices = {}
+GameServices.__index = GameServices
 
-GameServices.Services = {
+-- Tipagem
+export type ServiceTable = {
+	ConsoleReporter: typeof(require(script.Parent:WaitForChild("ConsoleReporter"))),
+	MemoryManager: typeof(require(script.Parent:WaitForChild("MemoryManager"))),
+	GuardClause: typeof(require(script.Parent:WaitForChild("GuardClause"))),
+	ConfigManager: typeof(require(script.Parent:WaitForChild("ConfigManager"))),
+}
+
+-- @desc Instância única de services, com carregamento imediato e seguro
+GameServices.Services: ServiceTable = {
 	ConsoleReporter = require(script.Parent:WaitForChild("ConsoleReporter")),
 	MemoryManager  = require(script.Parent:WaitForChild("MemoryManager")),
 	GuardClause    = require(script.Parent:WaitForChild("GuardClause")),
 	ConfigManager  = require(script.Parent:WaitForChild("ConfigManager")),
 }
+
+-- @desc Permite acessar serviços de forma tipada
+function GameServices:GetService<T>(name: string): T?
+	return self.Services[name] :: T?
+end
+
+-- @desc Permite reatribuir um service (somente se realmente necessário)
+function GameServices:SetService(name: string, service: any)
+	self.Services[name] = service
+end
 
 return GameServices
